68b8ec38d322926501b48c3b3850cb75683e1835
Anthony Hart <cidsolutions@yahoo.com>
2016-06-07 23:01:02 -0700

    User Story 3 - Before Merge of Branch

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,10 +3,21 @@ class Api::UsersController < ApiController
   before_action :authenticated?
 
   def index
-
-    users = User.all 
+    users = User.all
     render json: users, each_serializer: UserSerializer
-
   end
 
+def create
+  user = User.new(user_params)
+  if user.save
+    render json: user
+  else
+    rendor json: {errors: user.errors.full_messages }, status: :unprocessable_entity
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :password)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,16 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   namespace :api, defaults: { format: :json } do
-    resources :users
+    resources :users do
+      resources :lists
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
+
   end
 
   root 'welcome#index'
