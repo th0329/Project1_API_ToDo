7dd57aa8794e7cb9ee20a5e9b84f6f7036026fc7
Anthony Hart <cidsolutions@yahoo.com>
2016-06-13 21:27:15 -0700

    User Story 3 Complete

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -3,6 +3,22 @@ class Api::ItemsController < ApiController
   before_action :authenticated?
 
   def create
+
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      rendor json: {errors: item.errors.full_messages}, status: :unprocessable_entity
+    end
+
   end
+
+private
+
+def item_params
+
+  params.require(:item).permit(:body)
+
+end
 
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,21 +3,27 @@ class Api::UsersController < ApiController
   before_action :authenticated?
 
   def index
+
     users = User.all
     render json: users, each_serializer: UserSerializer
+    
   end
 
 def create
+
   user = User.new(user_params)
   if user.save
     render json: user
   else
     rendor json: {errors: user.errors.full_messages }, status: :unprocessable_entity
   end
+
 end
 
 private
 
 def user_params
   params.require(:user).permit(:username, :password)
+end
+
 end
